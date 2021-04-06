3ab5cfc12be66895e402658e886f5e4f943234a2
Azenga Kevin <azenga.kevin7@gmail.com>
2021-04-06 16:13:31 +0300

    Wip, designing the auth screen and partially hooking up the navigation logic

--- app/src/main/java/ke/co/academicplanner/ui/auth/LoginActivity.kt
+++ app/src/main/java/ke/co/academicplanner/ui/auth/LoginActivity.kt
@@ -1,12 +1,40 @@
 package ke.co.academicplanner.ui.auth
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import ke.co.academicplanner.R
+import ke.co.academicplanner.databinding.ActivityLoginBinding
+import ke.co.academicplanner.ui.HomeActivity
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initWidgets()
+    }
+
+    private fun initWidgets() {
+
+        binding.loginButton.setOnClickListener {
+
+            val intent = Intent(this@LoginActivity, HomeActivity::class.java).apply {
+                flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
+            }
+
+            startActivity(intent)
+        }
+
+        binding.registerPromptTv.setOnClickListener {
+
+            val intent = Intent(this@LoginActivity, RegisterActivity::class.java)
+
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/ke/co/academicplanner/ui/auth/RegisterActivity.kt
+++ app/src/main/java/ke/co/academicplanner/ui/auth/RegisterActivity.kt
@@ -2,11 +2,27 @@ package ke.co.academicplanner.ui.auth
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import ke.co.academicplanner.R
+import ke.co.academicplanner.databinding.ActivityRegisterBinding
 
 class RegisterActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityRegisterBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        initWidgets()
+    }
+
+    private fun initWidgets() {
+
+        binding.loginPromptTv.setOnClickListener {
+            //Finish the activity and take user to the previous activity or kill the application
+            finish()
+        }
     }
 }
