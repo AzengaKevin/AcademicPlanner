5fee018cb62d17c80eb6dcb24ef97bb5d2ad391c
Azenga Kevin <azenga.kevin7@gmail.com>
2021-04-09 21:37:29 +0300

    Designing deadline summary page

--- app/src/main/java/ke/co/academicplanner/data/AcademicPlannerConstants.kt
+++ app/src/main/java/ke/co/academicplanner/data/AcademicPlannerConstants.kt
@@ -3,4 +3,5 @@ package ke.co.academicplanner.data
 object AcademicPlannerConstants{
     const val EMAIL_COLLECTION = "emails"
     const val SCORES_COLLECTION = "scores"
+    const val DEADLINES_COLLECTION = "deadlines"
 }
--- app/src/main/java/ke/co/academicplanner/ui/fragments/DeadlineSummaryFragment.kt
+++ app/src/main/java/ke/co/academicplanner/ui/fragments/DeadlineSummaryFragment.kt
@@ -1,60 +1,49 @@
 package ke.co.academicplanner.ui.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import ke.co.academicplanner.R
+import androidx.fragment.app.Fragment
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import ke.co.academicplanner.databinding.FragmentDeadlineSummaryBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+private const val TAG = "DeadlineSummaryFrag"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [DeadlineSummaryFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class DeadlineSummaryFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
+    private lateinit var firebaseAuth: FirebaseAuth
+    private lateinit var firebaseDb: FirebaseFirestore
+
+    private lateinit var binding: FragmentDeadlineSummaryBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+
+        firebaseAuth = FirebaseAuth.getInstance()
+        firebaseDb = FirebaseFirestore.getInstance()
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_deadline_summary, container, false)
+        binding = FragmentDeadlineSummaryBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment DeadlineSummaryFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            DeadlineSummaryFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+        binding.viewButton.setOnClickListener {
+            binding.viewLayout.visibility = View.GONE
+            binding.actionLayout.visibility = View.VISIBLE
+        }
+
+        binding.actionButton.setOnClickListener {
+            binding.actionLayout.visibility = View.GONE
+            binding.viewLayout.visibility = View.VISIBLE
+        }
+
     }
 }
