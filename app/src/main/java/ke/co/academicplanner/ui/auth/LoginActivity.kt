037933a9349676e23cd4f428010f004cc5768acb
Azenga Kevin <azenga.kevin7@gmail.com>
2021-04-09 17:33:52 +0300

    Wip, connected to firebase and added the login and registration logic

--- app/src/main/java/ke/co/academicplanner/ui/auth/LoginActivity.kt
+++ app/src/main/java/ke/co/academicplanner/ui/auth/LoginActivity.kt
@@ -3,18 +3,29 @@ package ke.co.academicplanner.ui.auth
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.util.Patterns
+import android.view.View
+import com.google.firebase.auth.FirebaseAuth
 import ke.co.academicplanner.databinding.ActivityLoginBinding
 import ke.co.academicplanner.ui.HomeActivity
+
+private const val TAG = "RegisterActivity"
+
 
 class LoginActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityLoginBinding
+
+    private lateinit var firebaseAuth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        firebaseAuth = FirebaseAuth.getInstance()
 
         initWidgets()
     }
@@ -23,11 +34,47 @@ class LoginActivity : AppCompatActivity() {
 
         binding.loginButton.setOnClickListener {
 
-            val intent = Intent(this@LoginActivity, HomeActivity::class.java).apply {
-                flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
+            val email = binding.emailAddressField.text.toString()
+            val password = binding.passwordField.text.toString()
+
+            if (email.isEmpty()) {
+                binding.emailAddressField.error = "Email address required"
+                return@setOnClickListener
             }
 
-            startActivity(intent)
+            if (password.isEmpty()) {
+                binding.passwordField.error = "Password is required"
+                return@setOnClickListener
+            }
+
+            if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+                binding.emailAddressField.error = "Valid email address required"
+                return@setOnClickListener
+            }
+
+            binding.loginProgressbar.visibility = View.VISIBLE
+            firebaseAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener {
+                    if(it.isSuccessful){
+
+                        Log.i(TAG, "signInWithEmailAndPassword: success")
+
+                        binding.loginProgressbar.visibility = View.GONE
+
+                        val intent = Intent(this@LoginActivity, HomeActivity::class.java).apply {
+                            flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
+                        }
+
+                        startActivity(intent)
+
+                    }else{
+
+                        Log.e(TAG, "signInWithEmailAndPassword: failed", it.exception)
+
+                        binding.loginProgressbar.visibility = View.GONE
+                    }
+                }
+
         }
 
         binding.registerPromptTv.setOnClickListener {
