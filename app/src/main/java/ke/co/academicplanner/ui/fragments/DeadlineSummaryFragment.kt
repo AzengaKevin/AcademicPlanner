0f966134cc7bf13ee1eacf83deb3500a6d62104f
Azenga Kevin <azenga.kevin7@gmail.com>
2021-04-09 22:08:55 +0300

    Wip, stting deadling

--- app/src/main/java/ke/co/academicplanner/ui/fragments/DeadlineSummaryFragment.kt
+++ app/src/main/java/ke/co/academicplanner/ui/fragments/DeadlineSummaryFragment.kt
@@ -1,12 +1,15 @@
 package ke.co.academicplanner.ui.fragments
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
+import ke.co.academicplanner.data.AcademicPlannerConstants
 import ke.co.academicplanner.databinding.FragmentDeadlineSummaryBinding
 
 private const val TAG = "DeadlineSummaryFrag"
@@ -41,9 +44,75 @@ class DeadlineSummaryFragment : Fragment() {
         }
 
         binding.actionButton.setOnClickListener {
-            binding.actionLayout.visibility = View.GONE
-            binding.viewLayout.visibility = View.VISIBLE
+
+            val deadline = binding.deadlineField.text.toString()
+
+            if (deadline.isEmpty()) {
+                binding.deadlineField.error = "Deadline is required"
+                return@setOnClickListener
+            }
+
+            val data = mapOf(Pair("deadline", deadline))
+
+            binding.progressBar.visibility = View.VISIBLE
+
+            firebaseDb.collection(AcademicPlannerConstants.DEADLINES_COLLECTION)
+                .document(firebaseAuth.currentUser!!.uid)
+                .set(data)
+                .addOnCompleteListener {
+
+                    if (it.isSuccessful) {
+
+                        binding.progressBar.visibility = View.GONE
+                        Toast.makeText(requireContext(), "Deadline Set", Toast.LENGTH_SHORT).show()
+                        binding.actionLayout.visibility = View.GONE
+                        binding.viewLayout.visibility = View.VISIBLE
+
+                    } else {
+
+                        binding.progressBar.visibility = View.GONE
+                        Toast.makeText(
+                            requireContext(),
+                            "Deadline Setting Failed",
+                            Toast.LENGTH_SHORT
+                        ).show()
+
+                    }
+
+                }
+
+
         }
 
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        firebaseDb.collection(AcademicPlannerConstants.DEADLINES_COLLECTION)
+            .document(firebaseAuth.currentUser!!.uid)
+            .addSnapshotListener(requireActivity()) { documentSnapshot, exception ->
+
+                if (exception != null) {
+                    Log.e(TAG, "getting deadline: failed", exception)
+                    Toast.makeText(requireContext(), "A fatal error occurred", Toast.LENGTH_SHORT)
+                        .show()
+                    return@addSnapshotListener
+                }
+
+                if (documentSnapshot!!.exists()) {
+                    val deadline = documentSnapshot.get("deadline").toString()
+                    if (binding.viewLayout.visibility == View.VISIBLE) {
+                        binding.deadlineTv.text = deadline
+                    }
+                } else {
+                    Toast.makeText(requireContext(), "Deadline not yet set", Toast.LENGTH_SHORT)
+                        .show()
+                }
+            }
+    }
+
+    override fun onStop() {
+        super.onStop()
     }
 }
